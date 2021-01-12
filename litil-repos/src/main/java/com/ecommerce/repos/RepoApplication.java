848265a15d60586200989dfce688c19a232d1392
sai <gowtham.jee17@gmail.com>
2021-01-12 22:44:31 +0530

    Adding modules as dependency instead of boot applications

--- litil-repos/src/main/java/com/ecommerce/repos/RepoApplication.java
+++ litil-repos/src/main/java/com/ecommerce/repos/RepoApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan({"com.ecommerce.*"})
+//@SpringBootApplication
+//@ComponentScan({"com.ecommerce.*"})
 public class RepoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RepoApplication.class, args);
-	}
+	//public static void main(String[] args) {
+		//SpringApplication.run(RepoApplication.class, args);
+	//}
 
 }
--- litil-restaurant/src/main/java/com/ecommerce/restaurant/Main.java
+++ litil-restaurant/src/main/java/com/ecommerce/restaurant/Main.java
@@ -3,11 +3,11 @@ package com.ecommerce.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Main {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(Main.class, args);
+//	}
 	
 }
